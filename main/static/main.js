3099baf71778c66c1784dc6951f81bdc405dec52
Parker <Parker.middleton96@gmail.com>
2025-05-24 19:42:32 -0600

    Added some CSS with a decent theme

--- main/static/main.js
+++ main/static/main.js
@@ -1,7 +1,7 @@
 //Javascript here eventually
 
 //turns the navbar background green when it gets clicked. 
-let navbar = document.getElementsByTagName("p")[0];
-navbar.addEventListener("click",function (){
-	alert("nav bar was clicked");
-});
+// let navbar = document.getElementsByTagName("p")[0];
+// navbar.addEventListener("click",function (){
+// 	alert("nav bar was clicked");
+// });
