55bd94deed2f200e4009def16b018cea2bf5d58b
Parker <Parker.middleton96@gmail.com>
2025-09-21 21:06:57 -0600

    deploying

--- portfolioWebsite/settings.py
+++ portfolioWebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-74_!zar(*cfrh$tes5mb*e)60zcb4-*55%x*@-cc-elip016vk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['parker-middleton-portfolio.com', 'www.parker-middleton-portfolio.com', 'localhost']
+ALLOWED_HOSTS = ['parker-middleton-portfolio.com', 'www.parker-middleton-portfolio.com']
 
 STATIC_URL = '/static/'
 
